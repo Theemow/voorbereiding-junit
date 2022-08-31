543b54c89d96c342cf94d140c4d6a395c86ae602
Theemow <timo.kamermans@gmail.com>
2022-08-31 10:56:46 +0200

    extra test toegevoegd

--- src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
@@ -3,6 +3,15 @@ package nl.han.ica.oose.dea.testedfizzbuzz;
 public class FizzBuzzExecutor {
 
     public String execute(int i){
-        return null;
+        int fizz = 3;
+        int buzz = 5;
+        if(i % fizz == 0 && i % buzz == 0){
+            return "FizzBuzz";
+        } else if (i % fizz == 0){
+            return "Fizz";
+        } else if (i % buzz == 0){
+            return "Buzz";
+        }
+        return i + "";
     }
 }
--- src/test/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutorTest.java
+++ src/test/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutorTest.java
@@ -14,6 +14,42 @@ class FizzBuzzExecutorTest {
         var testValue = sut.execute(37);
 
         // Assert
-        Assertions.assertEquals(null, testValue);
+        Assertions.assertEquals("37", testValue);
+    }
+
+    @Test
+    void executeWithFizzTest(){
+        // Arrange
+        var sut = new FizzBuzzExecutor();
+
+        // Act
+        var testValue = sut.execute(18);
+
+        // Assert
+        Assertions.assertEquals("Fizz", testValue);
+    }
+
+    @Test
+    void executeWithBuzzTest(){
+        // Arrange
+        var sut = new FizzBuzzExecutor();
+
+        // Act
+        var testValue = sut.execute(250);
+
+        // Assert
+        Assertions.assertEquals("Buzz", testValue);
+    }
+
+    @Test
+    void executeWithFizzBuzzTest(){
+        // Arrange
+        var sut = new FizzBuzzExecutor();
+
+        // Act
+        var testValue = sut.execute(330);
+
+        // Assert
+        Assertions.assertEquals("FizzBuzz", testValue);
     }
 }
